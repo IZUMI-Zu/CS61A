de183ed7b465034530827edb81d705313056fcc5
Binshuo Zu <1795729364@qq.com>
2022-02-12 21:35:45 +0800

    finish hw06

--- HW/hw06/hw06.scm
+++ HW/hw06/hw06.scm
@@ -1,11 +1,25 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s))) 
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
 
-(define (sign val) 'YOUR-CODE-HERE)
+(define (sign val)
+  (cond
+  ((< val 0) -1)
+  ((= val 0) 0)
+  ((> val 0) 1)
+  )
+)
 
 (define (square x) (* x x))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  (if (= exp 1)
+    base
+    (if (even? exp) 
+      (square (pow base (/ exp 2)))
+      (* base (square (pow base (floor (/ exp 2)))))
+      )
+  ) 
+)
