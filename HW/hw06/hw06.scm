5d5e9730a93609bf764921c54a9606df72401549
Binshuo Zu <1795729364@qq.com>
2022-02-12 11:57:38 +0800

    Add lab10 hw06 hw07

---
+++ HW/hw06/hw06.scm
@@ -0,0 +1,11 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) 'YOUR-CODE-HERE)
+
+(define (caddr s) 'YOUR-CODE-HERE)
+
+(define (sign val) 'YOUR-CODE-HERE)
+
+(define (square x) (* x x))
+
+(define (pow base exp) 'YOUR-CODE-HERE)
---
+++ HW/hw07/hw07.scm
@@ -0,0 +1,23 @@
+(define (filter-lst fn lst) 'YOUR-CODE-HERE)
+
+; ;; Tests
+(define (even? x) (= (modulo x 2) 0))
+
+(filter-lst even? '(0 1 1 2 3 5 8))
+
+; expect (0 2 8)
+(define (interleave first second) 'YOUR-CODE-HERE)
+
+(interleave (list 1 5 3) (list 2 4 6))
+
+; expect (1 2 5 4 3 6)
+(interleave (list 1 3 5) nil)
+
+; expect (1 3 5)
+(interleave (list 1 3 5) (list 2 4))
+
+; expect (1 2 3 4 5)
+(define (accumulate combiner start n term)
+  'YOUR-CODE-HERE)
+
+(define (without-duplicates lst) 'YOUR-CODE-HERE)
